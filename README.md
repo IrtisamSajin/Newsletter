f4db1507c5eb57814e70ac224be4393e25367f76
Irtisam Sajin <irtisamcoding@gmail.com>
2021-11-18 10:48:33 +0600

    Version 1.0

---
+++ README.md
@@ -0,0 +1,24 @@
+# Newsletter Signup Page
+
+<h4>
+  A backend Node.js project that uses Mailchimp to store the email addresses of the users who want to subscribe to the Newsletter. The frontend is built using Bootstrap "Sign-in" template.
+  The backend uses Node.js with Express and NPM. NPM packages that have been used are: express, body-parser, https and request. The project has been hosted on Heroku: <a href="https://enigmatic-castle-41456.herokuapp.com/">Newsletter</a>
+</h4>
+
+<h4>
+  Below are some screenshots of the project:
+  </h4><br>
+  <h4>1. Homepage</h4>
+  <img src="screenshots/Homepage.png" height="400" width="800"><br>
+
+   <h4>2. Success page loads up when the user is successfully subscribed to the Newsletter</h4>
+  <img src="screenshots/Success.png" height="400" width="800"><br>
+
+   <h4>3. Failure page loads up when something goes wrong. The "Try Again" button redirects to the Homepage</h4>
+  <img src="screenshots/Failure.png" height="400" width="800"><br>
+
+ <h4>4. User can subscribe by filling out the form</h4>
+  <img src="screenshots/Submission.png" height="400" width="800"><br>
+
+ <h4>5. The Mailchimp dashboard which shows all the users who have subscribed along with their names and email id</h4>
+  <img src="screenshots/Email-list.png" height="400" width="800"><br>
---
+++ app.js
@@ -0,0 +1,69 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const request=require("request");
+const https=require("https");
+
+const app=express();
+app.use(express.static("Public"));  //For using static images and css file kept in Public folder
+app.use(bodyParser.urlencoded({extended:true}));  //for using body-parser
+
+
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/signup.html");
+})
+
+
+
+app.post("/", function(req,res){
+  var FirstName=req.body.FirstName;
+  var LastName=req.body.LastName;
+  var email=req.body.email;
+
+  var data={
+    members:[
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: FirstName,
+          LNAME: LastName
+        }
+      }
+    ]
+  }
+  var jsonData=JSON.stringify(data);
+  var url="https://us20.api.mailchimp.com/3.0/lists/"+"USERID"; //USERID is to be collected from Mailchimp
+  const options={
+    method: "Post",
+    auth: "irtisam:"+"API_KEY" //API Key is to be collected from Mailchimp
+  }
+
+  const request=https.request(url,options,function(response){
+    response.on("data",function(data){
+      if(response.statusCode===200){
+        res.sendFile(__dirname+"/success.html")
+      }
+      else{
+        res.sendFile(__dirname+"/failure.html")
+      }
+
+
+      console.log(JSON.parse(data));
+    })
+  })
+  request.write(jsonData)
+  request.end()
+
+
+})
+
+
+app.post("/failure",function(req,res){
+  res.redirect("/");
+})
+
+
+app.listen(process.env.PORT || 3000,function(){
+  console.log("Server is running on port 3000");
+})
